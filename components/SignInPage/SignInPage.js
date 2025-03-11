bbdd24c9eb4d22004bc5de618bb7b87f1e08f091
showaibbinnasir <showaibnishad@gmail.com>
2025-03-11 03:25:58 -0500

    login page updated

--- components/SignInPage/SignInPage.js
+++ components/SignInPage/SignInPage.js
@@ -7,25 +7,37 @@ export default function SignInPage() {
   const [email, setEmail] = useState("")
   const [password, setPassword] = useState("")
   const [error, setError] = useState("")
+  const [isLoading, setIsLoading] = useState(false)
+  const handleLogin = e => {
+    e.preventDefault()
+    setIsLoading(true)
+    if(!email || !password){
+      setIsLoading(false)
+      setError("All fields are required")
+      return
+    }else{
+      setError("")
+    }
+  }
   return (
     <div className='bg-[#D9D9D9] rounded-none lg:rounded-s-lg p-5 h-[350px] lg:h-[565px] flex items-center justify-center'>
       <div>
         <h1 className='font-[family-name:var(--font-sigmar)] text-center mt-5 text-4xl'>Welcome to Login Page</h1>
         <h1 className='font-[family-name:var(--font-sigmar)] text-center my-2 text-xl'>Login with your email and password</h1>
         <div>
-          <form>
+          <form onClick={handleLogin}>
             <fieldset className="w-[300px] lg:w-[400px] space-y-1">
               <Label htmlFor="name">Enter Email</Label>
-              <Input id="name" placeholder="Enter Email" type="email" />
+              <Input onChange={(e) => setEmail(e.target.value)} id="name" placeholder="Enter Email" type="email" />
             </fieldset>
             <fieldset className="w-[300px] lg:w-[400px] space-y-1">
               <Label htmlFor="name">Enter Password</Label>
-              <Input id="name" placeholder="Enter name" type="password" />
+              <Input onChange={(e) => setPassword(e.target.value)} id="name" placeholder="Enter Password" type="password" />
             </fieldset>
             {
               error && <div className='flex my-2'>
                 <div className=' text-white bg-red-500 px-5 py-1'>
-                  <h1>Error exists</h1>
+                  <h1>{error}</h1>
                 </div>
               </div>
             }
